e81beea7d111b4ab07696a0e52d8bce993d063b4
Eduardo Canche Vazquez <eduardo.942712@gmail.com>
2021-05-18 15:00:55 -0500

    added unique attribute on the custom tag name

--- models/web-component.js
+++ models/web-component.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const webComponentSchema = mongoose.Schema({
     filePath: { type: String, required: true },
-    customTagName: { type: String }
+    customTagName: { type: String, required: true, unique: true }
 });
 
 module.exports = mongoose.model('WebComponent', webComponentSchema);
