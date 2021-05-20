c3cf7f1465f392561b55a28d397d6646e4053c36
Eduardo Canche Vazquez <eduardo.942712@gmail.com>
2021-05-20 09:47:11 -0500

    The password shouldn't be unique

--- models/form-example.js
+++ models/form-example.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const formExampleSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     forwarders: [ { type: String } ],
     serverIp: { type: String }
 });
